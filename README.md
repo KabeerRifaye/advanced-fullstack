f071f09cf8ee6484b21b63afbba494787f5d1632
Kabeer Rifaye <citizen.kabeer@gmail.com>
2016-09-22 10:11:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+<!-- 21-Sep-2016 -->
+1 change the database name to your github username
+2 change the form to log first name, last name, and email address
+3 log a timestamp when users sign up
+
+<!-- 22-Sep-2016 -->
+4 require that fields are not empty
+5 check for valid email addresses
+6 display the total number of user signups at the top of /userlist
+7 display a thank you message after completing the sign up form instead of redirecting to /userlist (hint, use AJAX)
+8 disallow duplicate email addresses in the database
+9 display an “error, a signup with this email address already exists” message for duplicate email addresses
+
+10 set environmental variables for PORT and production/development mode with dotenv
+11 style the userlist. See [userlist.png](userlist.png) for inspiration. 
+12 add an option to csv export the user list  
+13 password protect /userlist -- do not store the password in the repository
---
+++ routes/index.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import monk from 'monk';
+
+const router = express.Router(); // TODO fix linter config or add eslint ignore
+const db = monk('localhost:27017/nodetest1');
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Add New User',
+  });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', (req, res) => {
+  const collection = db.get('usercollection');
+  collection.find({}, {}, (e, docs) => {
+    res.render('userlist', {
+      userlist: docs,
+    });
+  });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', (req, res) => {
+  // Get our form values. These rely on the "name" attributes
+  const firstName = req.body.firstname;
+  const lastName = req.body.lastname;
+  const userEmail = req.body.useremail;
+  const created_At = new Date();
+  console.log("created_At",created_At);
+
+  // Set our collection
+  const collection = db.get('usercollection');
+
+  // Submit to the DB
+  collection.insert({
+    firstname: firstName,
+    lastname: lastName,
+    email: userEmail,
+    created_at: created_At
+  }, (err) => {
+    if (err) {
+      // If it failed, return error
+      res.send('There was a problem adding the information to the database.');
+    } else {
+      res.redirect('userlist');
+    }
+  });
+});
+
+export default router;
